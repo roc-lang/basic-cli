2befcbddb236b008cde49b1f1499d2b93e4756df
Luke Boswell <lukewilliamboswell@gmail.com>
2024-01-14 08:18:26 +1100

    remove glue, add to CI

--- platform/glue.sh
+++ platform/glue.sh
@@ -9,6 +9,23 @@ rm -rf platform/glue
 echo "Generating glue files..."
 roc glue ../roc/crates/glue/src/RustGlue.roc platform/glue platform/main-glue.roc
 
-echo "NOTE: manually fix any errors in platform/glue files"
+# Manually fixup glue errors
+
+# Determine OS type
+if [[ "$(uname)" == "Darwin" ]]; then
+    # macOS (BSD $SED_COMMAND)
+    SED_COMMAND="sed -i ''"
+else
+    # Assuming Linux (GNU sed)
+    SED_COMMAND="sed -i"
+fi
+
+for file in platform/glue/roc_app/src/*.rs; do
+    $SED_COMMAND '/const _SIZE_CHECK_union_ReadErr/s/^/\/\//' "$file"
+    $SED_COMMAND '/const _SIZE_CHECK_union_ConnectErr/s/^/\/\//' "$file"
+    $SED_COMMAND '/const _SIZE_CHECK_union_StreamErr/s/^/\/\//' "$file"
+    $SED_COMMAND '/const _SIZE_CHECK_union_ReadErr/s/^/\/\//' "$file"
+    $SED_COMMAND '/const _SIZE_CHECK_union_WriteErr/s/^/\/\//' "$file"
+done
 
 
